97b494520efc0ad03b1344c32fe896e7169b0c83
Satoshi Hirata <s.hirata0507@gmail.com>
2020-04-12 00:22:41 +0900

    課題修正提出0412

---
+++ janken2.rb
@@ -0,0 +1,52 @@
+# require 'byebug'
+class Player
+    def hand
+     puts "数字を入力してください。"
+     puts "0:グー, 1:チョキ, 2:パー"
+       input_hand = gets.chomp
+      #  input_hand = input_hand.to_i
+        unless  input_hand ==  "0" || input_hand  == "1" || input_hand  == "2"
+          puts "もう一度数字を入力してください"
+          return hand
+        else
+          return input_hand.to_i
+        end    
+    end
+end
+  
+class Enemy
+    def hand
+      enemy_hand = rand(3)
+    end
+end
+  
+class Janken
+    def pon (player_hand, enemy_hand)
+      janken = ["グー", "チョキ", "パー"]
+      if (player_hand - enemy_hand +3) % 3 ==  0
+        puts "相手の手は#{janken[enemy_hand]}です。あいこです。"
+        return true
+      elsif (player_hand - enemy_hand +3) % 3 == 2
+        puts "相手の手は#{janken[enemy_hand]}です。あなたの勝ちです"
+        return false
+      elsif (player_hand - enemy_hand +3) % 3 == 1
+          # 「あなたの負けです」を出力します。
+        puts "相手の手は#{janken[enemy_hand]}です。あなたの負けです"
+        return false
+      else
+        puts "0~2の数字を入力してください"
+        return false
+      end
+    end        
+end  
+
+# binding.irb
+# # byebug
+  
+player = Player.new
+enemy = Enemy.new
+janken = Janken.new
+  
+#じゃんけんのメソッドが起動される
+janken.pon(player.hand, enemy.hand)
+
